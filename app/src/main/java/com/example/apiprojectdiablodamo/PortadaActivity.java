6793671914b691d7cdee90d808410a79f7abc1b0
etz00 <115375641+etz00@users.noreply.github.com>
2023-12-07 18:34:25 +0100

    Portada Activity

---
+++ app/src/main/java/com/example/apiprojectdiablodamo/PortadaActivity.java
@@ -0,0 +1,31 @@
+package com.example.apiprojectdiablodamo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class PortadaActivity extends AppCompatActivity {
+
+    private ImageView logoImage;
+    private Button IniciActivitat_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.portada);
+
+        logoImage=(ImageView) findViewById(R.id.logoImageView);
+        IniciActivitat_btn=(Button) findViewById(R.id.IniciActivitat_btn);
+
+        IniciActivitat_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PortadaActivity.this, SegonaActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
