2e73ed7efeebe80e8a7999f70465ed2d0c76356a
alexrocamora <72758624+alexrocamora@users.noreply.github.com>
2023-12-16 20:24:12 +0100

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        dataBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -36,4 +40,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Dependencias para el Navigation Component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.2")
 }
