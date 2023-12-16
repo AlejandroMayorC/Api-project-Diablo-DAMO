c20ad2056325481dee6376d2ed6dcfdaa7bc4365
alexrocamora <72758624+alexrocamora@users.noreply.github.com>
2023-12-16 18:03:23 +0100

    SegonaActivity con menú + Fragments
    Primera versión no funcional de la Activity tras entrar en la app y sus Fragments. Falta mejorar las versiones.

--- app/src/main/java/com/example/apiprojectdiablodamo/SegonaActivity.java
+++ app/src/main/java/com/example/apiprojectdiablodamo/SegonaActivity.java
@@ -1,14 +1,38 @@
 package com.example.apiprojectdiablodamo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.apiprojectdiablodamo.databinding.ActivitySegonaBinding;
+
 public class SegonaActivity extends AppCompatActivity {
+
+    private ActivitySegonaBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_segona);
+
+        binding = ActivitySegonaBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_chronology, R.id.navigation_acts, R.id.navigation_craft,
+                R.id.navigation_followers, R.id.navigation_character, R.id.navigation_item)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_segona);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
+
 }
---
+++ app/src/main/java/com/example/apiprojectdiablodamo/ui/ActsFragment.java
@@ -0,0 +1,62 @@
+package com.example.apiprojectdiablodamo.ui;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.apiprojectdiablodamo.R;
+
+public class ActsFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private Button buttonAll;
+    private Button buttonFavorites;
+    private TextView textViewTitle;
+
+    public ActsFragment() {
+        // Required empty public constructor
+    }
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_acts, container, false);
+
+        textViewTitle = root.findViewById(R.id.textViewTitle);
+        buttonAll = root.findViewById(R.id.buttonAll);
+        buttonFavorites = root.findViewById(R.id.buttonFavorites);
+        recyclerView = root.findViewById(R.id.recyclerView);
+
+        // Aquí se configuraría el RecyclerView con un LayoutManager y un Adapter
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        // Suponiendo que tienes un adaptador llamado MyAdapter
+        // recyclerView.setAdapter(new MyAdapter());
+
+        // Configurar los oyentes de los botones
+        buttonAll.setOnClickListener(view -> {
+            // Aquí manejarías el clic en el botón "Todos"
+        });
+
+        buttonFavorites.setOnClickListener(view -> {
+            // Aquí manejarías el clic en el botón "Favoritos"
+        });
+
+        // Establecer el título según la opción del menú seleccionada
+        // textViewTitle.setText("Acts");
+
+        return root;
+    }
+
+    // Aquí podrías incluir más métodos, por ejemplo, para actualizar la lista cuando se seleccionan los botones
+}
